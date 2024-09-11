784d82987fe3d5a35a86d08db076ee3f33688232
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-09-11 11:39:40 +0200

    Update exceptions to include frontend codes

--- appointment-center-rest/src/main/java/com/biit/appointment/rest/api/exceptions/AppointmentCenterExceptionControllerAdvice.java
+++ appointment-center-rest/src/main/java/com/biit/appointment/rest/api/exceptions/AppointmentCenterExceptionControllerAdvice.java
@@ -11,87 +11,88 @@ import com.biit.appointment.core.exceptions.ProfessionalSpecializationNotFoundEx
 import com.biit.appointment.core.exceptions.RecurrenceNotFoundException;
 import com.biit.appointment.core.exceptions.YouAreAlreadyOnThisAppointmentException;
 import com.biit.appointment.core.exceptions.YouAreNotOnThisAppointmentException;
+import com.biit.server.exceptions.ErrorResponse;
 import com.biit.server.exceptions.NotFoundException;
 import com.biit.server.exceptions.ServerExceptionControllerAdvice;
 import com.biit.server.logger.RestServerExceptionLogger;
-import org.modelmapper.spi.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class UserManagerExceptionControllerAdvice extends ServerExceptionControllerAdvice {
+public class AppointmentCenterExceptionControllerAdvice extends ServerExceptionControllerAdvice {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Object> notFoundException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("NOT_FOUND", ex), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse("NOT_FOUND", ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidRecurrenceException.class)
     public ResponseEntity<Object> invalidRecurrenceException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("Recurrence is not valid!", ex), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse("Recurrence is not valid!", "invalid_recurrence", ex), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RecurrenceNotFoundException.class)
     public ResponseEntity<Object> recurrenceNotFoundException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("Recurrence not found!", ex), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse("Recurrence not found!", "recurrence_not_found", ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ExaminationTypeNotFoundException.class)
     public ResponseEntity<Object> examinationTypeNotFoundException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("Examination type not found!", ex), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse("Examination type not found!", "examination_type_not_found", ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AppointmentTypeNotFoundException.class)
     public ResponseEntity<Object> appointmentTypeNotFoundException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("Appointment type not found!", ex), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse("Appointment type not found!", "appointment_type_not_found", ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AppointmentNotFoundException.class)
     public ResponseEntity<Object> appointmentNotFoundException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("Appointment not found!", ex), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse("Appointment not found!", "appointment_not_found", ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ProfessionalSpecializationNotFoundException.class)
     public ResponseEntity<Object> professionalSpecializationNotFoundException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("Professional Specialization not found!", ex), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse("Professional Specialization not found!", "professional_specialization_not_found", ex),
+                HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidParameterException.class)
     public ResponseEntity<Object> invalidParameterException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("Parameter invalid!", ex), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse("Invalid parameter!", "invalid_parameter", ex), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidFormatException.class)
     public ResponseEntity<Object> invalidFormatException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage(), ex), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "invalid_format", ex), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(YouAreNotOnThisAppointmentException.class)
     public ResponseEntity<Object> youAreNotOnThisAppointmentException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage(), ex), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "user_not_in_appointment", ex), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(YouAreAlreadyOnThisAppointmentException.class)
     public ResponseEntity<Object> youAreAlreadyOnThisAppointmentException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage(), ex), HttpStatus.CONFLICT);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "user_already_in_appointment", ex), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(AttendanceNotFoundException.class)
     public ResponseEntity<Object> attendanceNotFoundException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage(), ex), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "attendance_not_found", ex), HttpStatus.NOT_FOUND);
     }
 }
