34075b6ba861f56759dc31935583b8e52213107d
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-04-15 13:45:45 +0200

    Reviewed exception handling

--- appointment-center-rest/src/main/java/com/biit/appointment/rest/api/exceptions/AppointmentCenterExceptionControllerAdvice.java
+++ appointment-center-rest/src/main/java/com/biit/appointment/rest/api/exceptions/AppointmentCenterExceptionControllerAdvice.java
@@ -13,10 +13,14 @@ import com.biit.appointment.core.exceptions.RecurrenceNotFoundException;
 import com.biit.appointment.core.exceptions.YouAreAlreadyOnThisAppointmentException;
 import com.biit.appointment.core.exceptions.YouAreNotOnThisAppointmentException;
 import com.biit.appointment.persistence.exceptions.InvalidScheduleException;
+import com.biit.kafka.exceptions.InvalidEventException;
 import com.biit.server.exceptions.ErrorResponse;
 import com.biit.server.exceptions.NotFoundException;
 import com.biit.server.exceptions.ServerExceptionControllerAdvice;
 import com.biit.server.logger.RestServerExceptionLogger;
+import com.biit.usermanager.client.exceptions.ElementNotFoundException;
+import com.biit.usermanager.client.exceptions.InvalidConfigurationException;
+import com.biit.usermanager.client.exceptions.InvalidValueException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -108,5 +112,29 @@ public class AppointmentCenterExceptionControllerAdvice extends ServerExceptionC
     public ResponseEntity<Object> invalidProfessionalSpecializationException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
         return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "invalid_professional_specialization", ex), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidEventException.class)
+    public ResponseEntity<Object> invalidEventException(Exception ex) {
+        RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "cannot_connect_to_kafka", ex), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ElementNotFoundException.class)
+    public ResponseEntity<Object> elementNotFoundException(Exception ex) {
+        RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "not_found", ex), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidConfigurationException.class)
+    public ResponseEntity<Object> invalidConfigurationException(Exception ex) {
+        RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "invalid_configuration_exception", ex), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(InvalidValueException.class)
+    public ResponseEntity<Object> invalidValueException(Exception ex) {
+        RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "invalid_parameter", ex), HttpStatus.BAD_REQUEST);
     }
 }
