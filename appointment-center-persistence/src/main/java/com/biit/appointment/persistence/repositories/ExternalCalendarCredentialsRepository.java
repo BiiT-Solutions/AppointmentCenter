4e9e2fb81865b6f068ea447308bdd80cb2f505e8
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-05-19 08:40:51 +0200

    Fixing database access

--- appointment-center-persistence/src/main/java/com/biit/appointment/persistence/repositories/ExternalCalendarCredentialsRepository.java
+++ appointment-center-persistence/src/main/java/com/biit/appointment/persistence/repositories/ExternalCalendarCredentialsRepository.java
@@ -3,12 +3,14 @@ package com.biit.appointment.persistence.repositories;
 import com.biit.appointment.persistence.entities.CalendarProvider;
 import com.biit.appointment.persistence.entities.ExternalCalendarCredentials;
 import com.biit.server.persistence.repositories.ElementRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@Transactional
 @Repository
 public interface ExternalCalendarCredentialsRepository extends ElementRepository<ExternalCalendarCredentials, Long> {
 
