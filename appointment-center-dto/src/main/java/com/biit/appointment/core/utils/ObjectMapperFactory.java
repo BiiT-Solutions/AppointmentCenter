1ab49bfe2cf7898ffcbdf4f6ef25d7be16ca3c10
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-06-03 15:31:29 +0200

    Updated serializer

--- appointment-center-dto/src/main/java/com/biit/appointment/core/utils/ObjectMapperFactory.java
+++ appointment-center-dto/src/main/java/com/biit/appointment/core/utils/ObjectMapperFactory.java
@@ -11,8 +11,11 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 
 public final class ObjectMapperFactory {
+    private static final int SECONDS_MAX_DECIMALS = 9;
 
     private static ObjectMapper objectMapper;
 
@@ -22,7 +25,13 @@ public final class ObjectMapperFactory {
 
     public static ObjectMapper getNewObjectMapper() {
         final JavaTimeModule module = new JavaTimeModule();
-        final LocalDateTimeDeserializer localDateTimeDeserializer = new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS"));
+
+        final DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                .appendPattern("yyyy-MM-dd'T'HH:mm:ss")
+                .appendFraction(ChronoField.MILLI_OF_SECOND, 0, SECONDS_MAX_DECIMALS, true)
+                .toFormatter();
+
+        final LocalDateTimeDeserializer localDateTimeDeserializer = new LocalDateTimeDeserializer(formatter);
         module.addDeserializer(LocalDateTime.class, localDateTimeDeserializer);
         final ObjectMapper objectMapper = JsonMapper.builder()
                 .addModule(module)
